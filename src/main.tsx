be8b3d0693b4a3b051e2c223160cc26d4d7ba6ee
vinifreitassss <viniciusfreitas994@gmail.com>
2026-06-11 13:08:49 -0300

    Carrega CSS da aba de fotos

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./photo.css";
 
-createRoot(document.getElementById("root")!).render(
+const rootEl = document.getElementById("root");
+
+if (!rootEl) {
+  throw new Error("Elemento #root não encontrado");
+}
+
+createRoot(rootEl).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
